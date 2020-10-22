a40a912b9ff1e20a9a6264b95d0f0e57a8f409c9
root <root@9699a64f5c40>
2020-10-22 19:28:02 +0000

    Pop ups

---
+++ src/_03_print_and_popups/_1_greeter/Tester.java
@@ -0,0 +1,20 @@
+package _03_print_and_popups._1_greeter;
+
+import javax.swing.JOptionPane;
+
+public class Tester {
+public static void main(String[] args) {
+	
+	System.err.println("This is a test");
+	JOptionPane.showMessageDialog(null, "This is also a test");
+	
+	String answer = JOptionPane.showInputDialog("what is the anser?");
+	
+	if (answer.equalsIgnoreCase("no"));
+	
+	
+	
+	
+	
+}
+}
